d782d4ddaa4ad4611ad8af35c0b88a7f9da8ba52
otaki0413 <takida0413@gmail.com>
2023-09-10 03:30:52 +0900

    ✨ supabaseからpostsテーブルのデータ取得

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <div>テスト</div>;
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+export default async function Home() {
+  // クライアント作成
+  const supabase = createServerComponentClient({ cookies });
+  // postsテーブルからデータ取得
+  const { data: posts } = await supabase.from("posts").select();
+
+  return <pre>{JSON.stringify(posts, null, 2)}</pre>;
 }
